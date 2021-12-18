19aceeba9e293975238e4b3d7ceac11c74334bdd
wanggf <llzero54@foxmail.com>
2021-12-18 15:39:41 +0800

    V1.0.0

--- src/main/java/cn/wanggf/spring/javafx/support/ControllerCallback.java
+++ src/main/java/cn/wanggf/spring/javafx/support/ControllerCallback.java
@@ -1,24 +1,11 @@
 package cn.wanggf.spring.javafx.support;
 
 import javafx.util.Callback;
-import org.springframework.context.ApplicationContext;
-import org.springframework.util.Assert;
 
 /**
  * 控制器工厂回调
  *
  * @author wanggf
  */
-public class ControllerCallback<R> implements Callback<Class<? extends R>, R> {
-    private final ApplicationContext applicationContext;
-
-    public ControllerCallback(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
-
-    @Override
-    public R call(Class<? extends R> clazz) {
-        Assert.notNull(clazz, "No controller type specified.");
-        return applicationContext.getBean(clazz);
-    }
+public interface ControllerCallback<R> extends Callback<Class<? extends R>, R> {
 }
